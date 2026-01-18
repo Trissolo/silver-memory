15e583c306f606b5fcc8fc82dbd9db0627ae9aa6
Trissolo <00tristano00@gmail.com>
2026-01-18 17:16:35 +0100

    Add gimp .xcf and textures files

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -14,13 +14,39 @@ export class Game extends Scene
     {
         this.cameras.main.setBackgroundColor(0x00ff00);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
-
+       /*
         this.add.text(512, 384, `Make something fun!\nand share it with us:\nsupport@phaser.io\n${testdata.join("\n")}`, {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
-        }).setOrigin(0.5);
+        }).setOrigin(0.5);*/
+
+        let roomdata = this.cache.json.get('roomdata')
+        //console.log("ROOMDATA", roomdata)
+        let curr = roomdata[0]
+        //console.log("CURR:", curr, curr.frame + "1")
+        //console.log(this.textures)
+        for (let curr of roomdata)
+        {
+            if (curr.kind ===1)
+            {
+                continue
+            }
+            console.log(curr)
+            let immy = this.add.image(curr.x, curr.y, "atlas0", curr.frame + (curr.suffix? "1": "")+".png")
+            immy.setDepth(curr.kind);
+            if (curr.kind === 4)
+            {
+                console.log("Depth Sorted")
+                immy.setOrigin(0.5, 1);
+            }
+            else
+            {
+                immy.setOrigin(0);
+            }
+
+
+        }
 
         this.input.once('pointerdown', () => {
 
