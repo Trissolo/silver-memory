c34ab37b1ec8c536d16585ba353861dbcbea611f
Trissolo <00tristano00@gmail.com>
2026-01-29 17:30:33 +0100

    Some minor test

--- src/game/roomscripts/rs0.js
+++ src/game/roomscripts/rs0.js
@@ -1,7 +1,12 @@
 export default class rs0
 {
     // wrench
-    static 0(sprite){console.log(sprite.frame.name)}
+    static 0(sprite)
+    {
+        const aryvar = this.thingsJson[sprite.rid]?.skipCond;
+        this.setVariable(aryvar, 1);
+        sprite.setVisible(false);
+    }
 
     // coperchio
     static 1(sprite){console.log(sprite.frame.name)}
@@ -22,7 +27,11 @@ export default class rs0
     static 6(sprite){console.log(sprite.frame.name)}
 
     // button
-    static 7(sprite){console.log(sprite.frame.name)}
+    static 7(sprite)
+    {
+        //console.log(sprite.frame.name);
+        // this.visible_things.get(3).setVisible(false);
+    }
 
     // mensole
     static 8(sprite){console.log(sprite.frame.name)}
@@ -34,14 +43,21 @@ export default class rs0
     static 10(sprite)
     {
         console.log(sprite.frame.name)
-        const thing_json_data = this.currentThings[sprite.rid];
+        const thing_json_data = this.roomJson.things[sprite.rid];
         console.log("thing_json_data:", thing_json_data.suffix);
         this.toggleBit(thing_json_data.suffix);
         sprite.setFrame(`${thing_json_data.frame}${this.getVarFromArray(thing_json_data.suffix)}`);
     }
 
     // vasca
-    static 11(sprite){console.log(sprite.frame.name)}
+    static 11(sprite)
+    {
+        //const wrenchSkipVar = [...this.thingsJson[0].skipCond];
+        //wrenchSkipVar.pop();
+        //const variable = 
+        const {skipCond} = this.thingsJson[0]
+        console.log(sprite.frame.name, `WrenchStatus: ${skipCond} (${this.getVarFromArray(skipCond)})`)
+    }
 
     // tubo
     static 12(sprite){console.log(sprite.frame.name)}
