4aadb5bea33eb4e6ebbfb32fbc860ac92ab5bd70
Trissolo <00tristano00@gmail.com>
2026-02-11 09:03:50 +0100

    Add memo for global utility method

--- other/external_stuff/gimp_stuff/plugins/tris_game_properties_json_editor/banalpackage/modules/crossdisciplinary.py
+++ other/external_stuff/gimp_stuff/plugins/tris_game_properties_json_editor/banalpackage/modules/crossdisciplinary.py
@@ -26,4 +26,71 @@ class CrossDisciplinary():
             return [vcoords, ary[2]]
         else:
             raise ValueError(f"Array length ({l}) out of range!")
-        
+    @classmethod
+    def add_button(cls):
+        print("Adding button")
+
+
+'''
+#temp reference:
+test_dialog = GimpUi.Dialog.new()
+test_dialog.add_button("_Canc", Gtk.ResponseType.CANCEL)
+sub_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 2)
+our_content_area = test_dialog.get_content_area()
+our_content_area.pack_start(sub_box, True, True, 1)
+
+def add_custom_button(icon_name, container, action):
+  newbutton = GimpUi.Button.new_from_icon_name(icon_name, 1)
+  newbutton.connect("clicked", action)
+  newbutton.set_halign(1)
+  newbutton.set_valign(1)
+  container.pack_start(newbutton, False, False, 2)
+  return newbutton
+
+def add_standard_label(container, name = f"A standard Label"):
+  l = Gtk.Label.new(name)
+  container.pack_start(l, True, True, 2)
+  return l
+
+def generic_on_click(button):
+  print(f"pressed {button.get_name()}")
+
+def another_on_click(button):
+  print("This is a different method")
+
+
+# [GimpUi.ICON_CHAR_PICKER, GimpUi.ICON_CLOSE, GimpUi.ICON_CLOSE_ALL, GimpUi.ICON_COLOR_PICK_FROM_SCREEN]
+for elem in [GimpUi.ICON_CAP_BUTT, GimpUi.ICON_CAP_ROUND, GimpUi.ICON_CAP_SQUARE, GimpUi.ICON_CENTER]:
+  add_custom_button(elem, sub_box, generic_on_click)
+
+for elem in [GimpUi.ICON_CHAR_PICKER, GimpUi.ICON_CLOSE, GimpUi.ICON_CLOSE_ALL, GimpUi.ICON_COLOR_PICK_FROM_SCREEN]:
+  add_custom_button(elem, sub_box, generic_on_click)
+
+label = add_standard_label(sub_box)
+
+#Sì! Lo centra
+sub_box.set_center_widget(label)
+sub_box.reorder_child(label, 3)
+
+our_content_area.show()
+test_dialog.show_all()
+
+#print("LABEL IDX", sub_box.get_children().index(label))
+#test_dialog.destroy()
+
+
+# Gtk.PackType.END
+# for idx, w in enumerate(sub_box.get_children()):
+#   expand, fill, padding, pack_type = sub_box.query_child_packing(w)
+#   print(expand, fill, padding, pack_type, w.__class__.__name__)
+#   if w.__class__.__name__ == "Label":
+#     must_move=True
+#   if must_move:
+#     print(f"Moving [{idx}]...")
+#     print("Before", sub_box.query_child_packing(w)[3])
+#     sub_box.set_child_packing(w, expand, fill, padding, Gtk.PackType.END)
+#     print("AFTER", sub_box.query_child_packing(w)[3])
+
+
+#test_dialog.destroy()
+'''
---
+++ src/game/roomscripts/rs2.js
@@ -0,0 +1,38 @@
+export default class rs2
+{
+    // button
+    static 0(thing)
+    {
+        //this.cameras.main.startFollow(new Phaser.Math.Vector2(250, 85));
+        
+        const ponte = this.getExistentThing(2);
+        console.log(thing.frame.name, ponte.frame.name);
+        const vcoords = ponte.rdata.skipCond[0];
+        console.log(`Ponte suffix: ${this.getVarValue(vcoords)}`, vcoords);
+        this.toggleBit(ponte.rdata.skipCond);
+        console.log(`Ponte suffix: ${this.getVarValue(vcoords)}`);
+        ponte.setVisible(!!this.getVarValue(ponte.rdata.skipCond[0]));
+    }
+
+    // portafake
+    static 1(thing){console.log(thing.frame.name);}
+
+    // ponte
+    static 2(thing){console.log(thing.frame.name);}
+
+    // siglight
+    static 3(thing){console.log(thing.frame.name);}
+
+    // AREA
+    static 4(thing){console.log(thing.frame.name);}
+
+    // AREA
+    static 5(thing){console.log(thing.frame.name);}
+
+    // muro
+    static 6(thing){console.log(thing.frame.name);}
+
+    // AREA
+    static 7(thing){console.log(thing.frame.name);}
+
+}
