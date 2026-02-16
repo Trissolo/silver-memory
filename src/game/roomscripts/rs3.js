a862bc6dc71fc7ac6a64c05fbbd6e9f6e8d4adbf
Trissolo <00tristano00@gmail.com>
2026-02-16 06:25:49 +0100

    Add the forgotten room3 script

---
+++ src/game/roomscripts/rs3.js
@@ -0,0 +1,12 @@
+export default class rs3
+{
+    // AREA
+    static 0(thing){console.log(thing.frame.name);}
+
+    // AREA
+    static 1(thing){console.log(thing.frame.name);}
+
+    // AREA
+    static 2(thing){console.log(thing.frame.name);}
+
+}
