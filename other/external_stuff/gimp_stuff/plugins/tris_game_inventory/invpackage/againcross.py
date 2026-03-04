9f55f18cf694392961cde4711738ab5f2ae30513
Trissolo <00tristano00@gmail.com>
2026-03-04 07:08:56 +0100

    Prepare the Gimp Plugin for the game inventory

---
+++ other/external_stuff/gimp_stuff/plugins/tris_game_inventory/invpackage/againcross.py
@@ -0,0 +1,225 @@
+import gi
+gi.require_version('Gimp', '3.0')
+from gi.repository import Gimp
+
+class ImageStuff():
+    def __init__(self, *, image, **kwargs):
+        super().__init__(**kwargs)
+        self.image = image
+        self.layer = None
+    def remove_image_references(self):
+        self.image = None
+        self.layer = None
+    def update_layer(self):
+        '''Set the currently selected layer as active layer'''
+        self.layer = self.image.get_selected_layers()[0]
+    def select_adjacent_layer(self, button):
+        '''This method assumes that the argument 'button' is an Object with the property 'dir' set to 1 or -1.'''
+        #print(f":( Expensive: {button.dir}")
+        layers = self.image.get_layers()
+        layers_length = len(layers)
+        if not self.layer or layers_length < 2:
+            print("No next layer. Skipping :(")
+            return
+        next_idx = layers.index(self.layer) + button.dir
+        self.image.set_selected_layers([layers[0] if next_idx == layers_length else layers[next_idx]])
+        # call 'update_layer' as temporary behavior:
+        self.update_layer()
+    def attach_array_to_current_layer(self, parasite_name, integers_array):
+        '''Creates a parasite with the given name and attaches it to the current layer.
+        If a parasite with the same name already exists, it will be eliminated before the process.'''
+        # If an old parasite with the same name already exists, detach it:
+        if parasite_name in self.layer.get_parasite_list():
+            self.layer.detach_parasite(parasite_name)
+        # convert ary to string. i.e. [300, 150, 10] -> "300 150 10"
+        stringa_dati = " ".join(map(str, integers_array)) 
+        # Crea il parassita. 
+        # In GIMP 3, i dati devono essere passati come bytes o GLib.Bytes
+        dati_bytes = stringa_dati.encode('ascii')
+        # Finally creates the parasite and attach it
+        new_parasite = Gimp.Parasite.new(parasite_name, Gimp.PARASITE_PERSISTENT, dati_bytes)       
+        self.layer.attach_parasite(new_parasite)
+    def extract_array_from_parasite(self, parasite_name, layer = None):
+        '''Returns the array contained in the parasite identified by the given name.
+        Or an empty array if the parasite does not exist.'''
+        if layer is None:
+            layer = self.layer
+        parasite = layer.get_parasite(parasite_name)
+        if not parasite:
+            print(f"No parasite with name: {parasite_name}.")
+            return []
+        # 1. Ottieni i byte e decodificali in stringa "300 150 10"
+        #raw_data = parasite.get_data()
+        stringa_dati = bytes(parasite.get_data()).decode('ascii')  
+        # 2. Trasforma la stringa in una lista di interi [300, 150, 10]
+        try:
+            integers_array = [int(n) for n in stringa_dati.split()]
+            return integers_array
+        except ValueError:
+            return []
+    def detach_parasite_from_current_layer(self, parasite_name):
+        '''If the current layer contains a parasite identified by the specified name, that parasite is deleted.'''
+        if parasite_name in self.layer.get_parasite_list():
+            self.layer.detach_parasite(parasite_name)
+    def get_owned_parasites(self, layer = None):
+        if layer is None:
+            layer = self.layer
+        return layer.get_parasite_list()
+
+
+'''
+from collections import namedtuple
+
+FakeWidget = namedtuple("FakeWidget", "dir")
+
+button_next = FakeWidget(1)
+button_prev = FakeWidget(-1)
+test = ImageStuff(image = Gimp.get_images()[0])
+'''
+
+'''
+image
+layer
+update_layer
+remove_image_references
+select_adjacent_layer
+attach_array_to_current_layer
+detach_parasite_from_current_layer
+extract_array_from_parasite
+get_owned_parasites
+'''
+
+'''
+class ImageStuff():
+    def __init__(self, *, image, **kwargs):
+        super().__init__(**kwargs)
+        self.image = image
+        self.layer = None
+    def remove_image_references(self):
+        self.image = None
+        self.layer = None
+    def update_layer(self):
+        #Set the currently selected layer as active layer
+        self.layer = self.image.get_selected_layers()[0]
+    def attach_array_to_layer(self, parasite_name, integers_array):
+        if parasite_name in self.layer.get_parasite_list():
+            self.layer.detach_parasite(parasite_name)
+        # convert ary to string. i.e. [300, 150, 10] -> "300 150 10"
+        stringa_dati = " ".join(map(str, integers_array)) 
+        # Crea il parassita. 
+        # In GIMP 3, i dati devono essere passati come bytes o GLib.Bytes
+        dati_bytes = stringa_dati.encode('ascii')
+        nuovo_parassita = Gimp.Parasite.new(parasite_name, Gimp.PARASITE_PERSISTENT, dati_bytes)       
+        self.layer.attach_parasite(nuovo_parassita)
+    def extract_parasite_data(self, parasite_name, layer = None):
+        
+        parasite = self.layer.get_parasite(parasite_name)
+        if not parasite:
+            print(f"No parasite with name: {parasite_name}.")
+            return []
+        # 1. Ottieni i byte e decodificali in stringa "300 150 10"
+        raw_data = parasite.get_data()
+        stringa_dati = bytes(raw_data).decode('ascii')  
+        # 2. Trasforma la stringa in una lista di interi [300, 150, 10]
+        try:
+            integers_array = [int(n) for n in stringa_dati.split()]
+            return integers_array
+        except ValueError:
+            return []
+
+test = ImageStuff(image = Gimp.get_images()[0])
+'''
+'''
+#temp reference:
+test_dialog = GimpUi.Dialog.new()
+test_dialog.add_button("_Canc", Gtk.ResponseType.CANCEL)
+sub_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 2)
+our_content_area = test_dialog.get_content_area()
+our_content_area.pack_start(sub_box, True, True, 1)
+
+def add_custom_button(icon_name, container, action):
+  newbutton = GimpUi.Button.new_from_icon_name(icon_name, 1)
+  newbutton.connect("clicked", action)
+  newbutton.set_halign(1)
+  newbutton.set_valign(1)
+  container.pack_start(newbutton, False, False, 2)
+  return newbutton
+
+def add_standard_label(container, name = f"A standard Label"):
+  l = Gtk.Label.new(name)
+  container.pack_start(l, True, True, 2)
+  return l
+
+def generic_on_click(button):
+  print(f"pressed {button.get_name()}")
+
+def another_on_click(button):
+  print("This is a different method")
+
+
+# [GimpUi.ICON_CHAR_PICKER, GimpUi.ICON_CLOSE, GimpUi.ICON_CLOSE_ALL, GimpUi.ICON_COLOR_PICK_FROM_SCREEN]
+for elem in [GimpUi.ICON_CAP_BUTT, GimpUi.ICON_CAP_ROUND, GimpUi.ICON_CAP_SQUARE, GimpUi.ICON_CENTER]:
+  add_custom_button(elem, sub_box, generic_on_click)
+
+for elem in [GimpUi.ICON_CHAR_PICKER, GimpUi.ICON_CLOSE, GimpUi.ICON_CLOSE_ALL, GimpUi.ICON_COLOR_PICK_FROM_SCREEN]:
+  add_custom_button(elem, sub_box, generic_on_click)
+
+label = add_standard_label(sub_box)
+
+#Sì! Lo centra
+sub_box.set_center_widget(label)
+sub_box.reorder_child(label, 3)
+
+our_content_area.show()
+test_dialog.show_all()
+
+#print("LABEL IDX", sub_box.get_children().index(label))
+#test_dialog.destroy()
+
+
+# Gtk.PackType.END
+# for idx, w in enumerate(sub_box.get_children()):
+#   expand, fill, padding, pack_type = sub_box.query_child_packing(w)
+#   print(expand, fill, padding, pack_type, w.__class__.__name__)
+#   if w.__class__.__name__ == "Label":
+#     must_move=True
+#   if must_move:
+#     print(f"Moving [{idx}]...")
+#     print("Before", sub_box.query_child_packing(w)[3])
+#     sub_box.set_child_packing(w, expand, fill, padding, Gtk.PackType.END)
+#     print("AFTER", sub_box.query_child_packing(w)[3])
+
+
+#test_dialog.destroy()
+
+import gi
+gi.require_version('Gimp', '3.0')
+from gi.repository import Gimp
+
+def salva_dati_layer(layer, lista_interi):
+    # Converte [300, 150, 10] -> "300 150 10"
+    stringa_dati = " ".join(map(str, lista_interi))
+    
+    # Crea il parassita. 
+    # In GIMP 3, i dati devono essere passati come bytes o GLib.Bytes
+    dati_bytes = stringa_dati.encode('utf-8')
+    new_parasite = Gimp.Parasite.new("mio_gioco_dati", Gimp.PARASITE_PERSISTENT, dati_bytes)
+    
+    layer.attach_parasite(new_parasite)
+
+def leggi_dati_layer(layer):
+    parasite = layer.get_parasite("mio_gioco_dati")
+    if not parasite:
+        return []
+
+    # 1. Ottieni i byte e decodificali in stringa "300 150 10"
+    raw_data = parasite.get_data()
+    stringa_dati = bytes(raw_data).decode('utf-8')
+    
+    # 2. Trasforma la stringa in una lista di interi [300, 150, 10]
+    try:
+        lista_interi = [int(n) for n in stringa_dati.split()]
+        return lista_interi
+    except ValueError:
+        return []
+'''
---
+++ other/external_stuff/gimp_stuff/plugins/tris_game_inventory/tris_game_inventory.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#   GIMP - The GNU Image Manipulation Program
+#   Copyright (C) 1995 Spencer Kimball and Peter Mattis
+#
+#   *** Based on: ***
+#   https://testing.docs.gimp.org/3.0/en/gimp-using-python-plug-in-tutorial.html
+#   gimp-tutorial-plug-in.py
+#   sample plug-in to illustrate the Python plug-in writing tutorial
+#
+#   (ensure the script is executable)
+
+# sys: just for sys.argv and sys.path[0]
+import sys
+
+import gi
+
+gi.require_version("Gimp", "3.0")
+from gi.repository import Gimp
+
+gi.require_version("GimpUi", "3.0")
+from gi.repository import GimpUi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+# Glib is used mostly for GLib.Error()
+# or utility functions like:
+# GLib.build_pathv(GLib.DIR_SEPARATOR_S, [GLib.get_home_dir(), "my_file_name.txt"])
+from gi.repository import GLib
+
+# GObject is for GObject.ParamFlags:
+# G_PARAM_READABLE: 1
+# G_PARAM_WRITABLE: 2
+# G_PARAM_READWRITE: 3 # -> Alias for: G_PARAM_READABLE | G_PARAM_WRITABLE
+from gi.repository import GObject
+
+# Gio: I/O and files, e.g., new_file = Gio.File.new_for_path("/home/USER/Graphics/my_picture.xcf")
+#from gi.repository import Gio
+
+
+# other custom imports
+#import json
+#import os
+
+
+# our plugin class
+class GameInventory(Gimp.PlugIn):
+    # procedure(s) name in Procedure Browser. Note that this string value CANNOT have underscores, only hyphens/dashes
+    def do_query_procedures(self):
+        return ["tris-game-properties-json-editor"]
+
+
+    def do_set_i18n(self, name):
+        return False
+
+
+    # The ImageProcedure (mostly hardcoded strings)
+    def do_create_procedure(self, name):
+        procedure = Gimp.ImageProcedure.new(
+            self, name, Gimp.PDBProcType.PLUGIN, self.run, None
+        )
+
+        procedure.set_image_types("*")
+
+        procedure.set_menu_label("[Tris] ⛏️Tris Game Inventory generator")
+
+        procedure.add_menu_path("<Image>/Filters/[[Tris]]/")
+
+        procedure.set_documentation(
+            "[Tris] Basically, a barebone Javascript Class for a simple inventory in adventure games.",
+            "A complex plugin (GIMP 3.0). WIP",
+            name,
+        )
+
+        procedure.set_attribution("Tris", "---", "2026")
+        return procedure
+    
+
+    def procedure_is_complete(self, prcdr):
+        #Gimp.PDBStatusType
+        #       .EXECUTION_ERROR # == 0
+        #       .CALLING_ERROR # == 1
+        #       .PASS_THROUGH # Pass through == 2
+        #       .SUCCESS # Success == 3
+        #       .CANCEL # User cancel == 4
+        print("** Json procedure complete! :D **")
+        return prcdr.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
+    
+    # # Two test methods
+    # def set_working_directory(self):
+    #     GLib.chdir(GLib.path_get_dirname(__file__))
+    #     GLib.chdir("../..")
+    #     return GLib.get_current_dir()
+       
+    def run(self, procedure, run_mode, image, drawables, config, run_data):
+        print("** Starting -from scratch!- Json procedure **")
+        # just for debug: not required for the plugin purpose
+        # MessageHandlerType.MESSAGE_BOX = 0, CONSOLE = 1, ERROR_CONSOLE = 2
+        Gimp.message_set_handler(Gimp.MessageHandlerType.CONSOLE)
+
+        # quick bail
+        if len(image.get_layers()) == 0 or image.get_xcf_file() is None:
+            print("Quitting because there are no layers, or the image is not saved to disk...", sys.version, sys.version_info)
+            return self.procedure_is_complete(procedure)
+        
+        # initialize Gtk!
+        GimpUi.init(GLib.path_get_basename(__file__).removesuffix(".py"))
+
+        
+        print("*** Generate Game Inventory Plugin ***")
+
+        # from .invpackage.againcross import AgainCross
+
+        # q = AgainCross()
+        
+        # from banalpackage import MainDialog
+        # dialog = MainDialog()
+        # dialog.provide_image(image)
+        # dialog.run()
+        # dialog.destroy()
+
+        print("DESTROYED")
+        #print(GLib.path_get_dirname(__file__))
+        
+        '''
+        {
+            "kind": "ab",
+            "x": 90,
+            "y": 68,
+            "frame": "r2cabinetDoors",
+            "suffix": [0, 7],
+            "skipCond": "b_4_1",
+            "hoverName": 8,
+            "animation": "blink",
+            "no_interaction": True
+        }
+        '''
+        
+        # We have reached the end of the procedure: let's return "Success"
+        return self.procedure_is_complete(procedure)  #procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
+
+Gimp.main(GameInventory.__gtype__, sys.argv)
+
