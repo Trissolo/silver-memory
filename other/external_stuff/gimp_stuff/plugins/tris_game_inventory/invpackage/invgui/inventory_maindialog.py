dd716c3adae523f98875ee33f91f54a66d313b1d
Trissolo <00tristano00@gmail.com>
2026-03-16 04:31:07 +0100

    Add initial widget in Stack. Also, rename constants

--- other/external_stuff/gimp_stuff/plugins/tris_game_inventory/invpackage/invgui/inventory_maindialog.py
+++ other/external_stuff/gimp_stuff/plugins/tris_game_inventory/invpackage/invgui/inventory_maindialog.py
@@ -37,10 +37,10 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         self.curr_sel = None
         self.row_infos = None
 
-        self.stack = None
-        self.tw = None
+        #3 the Stack (empty for now)
+        self.stack = Gtk.Stack.new()
+        self.tw = Gtk.TreeView.new()
 
-        #3
 
         #4 Populate the Top Bar
         self.generate_top_bar()
@@ -48,47 +48,32 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         #5 Populate the Middle Bar
         self.generate_middle_bar()
 
-        # the Paned!
-        paned = self._gui_element_paned()
-        stack = Gtk.Stack.new()
-        paned.pack2(stack)
-        self.prepare_rowInfos(stack=stack)
-        tw = self.build_tw()
-        paned.pack1(tw)
-        paned.show_all()
-        #print(stack.get_children())
-        self.stack = stack
-
         #6 the core widgets!
-        # ...
-        
+        self.prepare_rowInfos(stack=self.stack)
 
-        #5 Set the Layer!
+        #7 the TreeView
+        tw = self.build_tw()
 
-        # self.update_layer()
+        #8 initial message on stack
+        l = Gtk.Label("Select a property")
+        l.show()
+        self.stack.add_named(l, "startmessage")
+        self.stack.set_visible_child_name("startmessage")
 
+        #9 the Paned!
+        paned = self._gui_element_paned()
+        paned.pack1(tw)
+        paned.pack2(self.stack)
+        paned.show_all()
 
         # test:
         print("Test: 'Dialog content area' children")
         for child in self.get_content_area().get_children():
             print(child.get_name())
 
-        #6 Ready!
-        
-
-        #self.curr_sel = self.row_infos[3]
-        #self.build_chooser_overnames(stack)
-        #self.build_chooser_vars(stack)
-
-        
-
-        
-        
+        #10 Ready!
         self.update_layer()
-        # self.tw_refresh_hard()
-        # self.summary_debug()
-        # self.generate_json()
-        # self.load_json_hovernames()
+        self.curr_sel = self.row_infos[0]
 
         # END __init__ method
     
@@ -102,6 +87,7 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         self.remove_image_references()
 
         self._top_label = None
+        self.stack = None
 
         for elem in self.row_infos:
             elem.clear()
@@ -110,7 +96,7 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         print("Inventory plugin destroyed!")
     
     def build_tw(self):
-        row_infos = self.row_infos # = self.prepare_rowInfos()
+        row_infos = self.row_infos
 
         self.curr_sel = None
 
@@ -132,19 +118,19 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         store = Gtk.ListStore.new(cell_types)
 
         # the TreeView
-        tw = Gtk.TreeView.new()
+        tw = self.tw
 
         tw.set_model(store)
 
         # constants AS!
-        tw.text_empty = "---"
-        tw.color_empty = "#343434"
-        tw.color_selected = "#ff0"
-        tw.color_set = "#66a"
+        self.CONST_TEXT_EMPTY = "---"
+        self.CONST_COLOR_EMPTY = "#343434"
+        self.CONST_COLOR_SELECTED = "#ff0"
+        self.CONST_COLOR_SET = "#66a"
 
         # populate the store:
         for row in row_infos:
-            store.append([row.prop, tw.text_empty, tw.text_empty, tw.color_empty, tw.color_empty])
+            store.append([row.prop, self.CONST_TEXT_EMPTY, self.CONST_TEXT_EMPTY, self.CONST_COLOR_EMPTY, self.CONST_COLOR_EMPTY])
         
         cell = Gtk.CellRendererText.new()
 
@@ -156,25 +142,27 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         tw.connect('row-activated', self.on_active_row)
         # paned_container.pack1(tw)
         tw.show_all()
-        self.tw = tw
         return tw
         
     def on_active_row(self, treeview, row_idx, colu):
         print("On Active Row!")
         index_as_int = row_idx.get_indices()[0]
         treeview.get_selection().unselect_all()
-        self.curr_sel = self.row_infos[index_as_int]
-        print(f"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq {self.curr_sel.wid=}")
-        self.stack.set_visible_child(self.curr_sel.wid)
+        #self.curr_sel = self.row_infos[index_as_int]
+        
 
         # Additional tests:
         model = treeview.get_model()
         if self.curr_sel is not None:
             model[self.curr_sel.row_idx][3] = model[self.curr_sel.row_idx][4]
-        model[index_as_int][3] = treeview.color_selected
-        model[index_as_int][4] = treeview.color_empty
-        print(f"CurrSel: {self.curr_sel}")
-        print(f"Analogies? {index_as_int=} -> {self.curr_sel.row_idx} -*- {model[row_idx][0]=} -> {self.curr_sel.prop} {model[row_idx][0]}")
+        model[index_as_int][3] = self.CONST_COLOR_SELECTED
+        model[index_as_int][4] = self.CONST_COLOR_EMPTY
+        
+        # important
+        self.curr_sel = self.row_infos[index_as_int]
+        self.stack.set_visible_child(self.curr_sel.wid)
+        #print(f"CurrSel: {self.curr_sel}")
+        #print(f"Analogies? {index_as_int=} -> {self.curr_sel.row_idx} -*- {model[row_idx][0]=} -> {self.curr_sel.prop} {model[row_idx][0]}")
     
     def tw_refresh_hard(self, tw=None, model=None):
         if tw == None:
@@ -184,48 +172,48 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         #print(f"{props=}")
         for row in model:
             if row[0] in props:
-                row[3] = row[4] = tw.color_set
+                row[3] = row[4] = self.CONST_COLOR_SET
                 ary = self.extract_array_from_parasite(row[0])
                 row[1] = f"{ary}"
             else:
-                for idx, value in enumerate([tw.text_empty, tw.text_empty, tw.color_empty, tw.color_empty], start=1):
+                for idx, value in enumerate([self.CONST_TEXT_EMPTY, self.CONST_TEXT_EMPTY, self.CONST_COLOR_EMPTY, self.CONST_COLOR_EMPTY], start=1):
                    #print(f"row[{idx}] = {value}")
                    row[idx]=value
         return True
     
     def prepare_rowInfos(self, stack):
-        wid_kind = self.build_chooser_kind(isMonoChooser=False, stack_container=stack)
-        wid_unary = self.build_chooser_kind(isMonoChooser=True, stack_container=stack)
-        wid_wars = self.build_chooser_vars(stack)
-        wid_hevernames = self.build_chooser_overnames(stack)
+        wid_kind = self.build_chooser_kind(isMonoChooser=False)
+        wid_unary = self.build_chooser_kind(isMonoChooser=True)
+        widget_vars = self.build_chooser_vars()
+        widget_overnames = self.build_chooser_overnames()
 
         json_props_size = (
             ('kind', 1, wid_kind),
-            ('hoverName', 1, wid_hevernames),
-            ('suffix', 2, wid_wars),
-            ('skipCond', 3, wid_wars),
+            ('hoverName', 1, widget_overnames),
+            ('suffix', 2, widget_vars),
+            ('skipCond', 3, widget_vars),
             ('noInteraction', 1, wid_unary),
-            ('roomStatus', 2, wid_wars),
-            ('roomVariable', 2, wid_wars)
+            ('roomStatus', 2, widget_vars),
+            ('roomVariable', 2, widget_vars)
         )
-        print("Debug core_stuff")
-        [print(prop_name, size, wid) for idx, (prop_name, size, wid) in enumerate(json_props_size)]
-        core_stuff = [SelectionInfo(prop_name, size, idx, wid) for idx, (prop_name, size, wid) in enumerate(json_props_size)]
+        
+        # print("Debug row_infos")
+        # [print(prop_name, size, wid) for idx, (prop_name, size, wid) in enumerate(json_props_size)]
+        row_infos = [SelectionInfo(prop_name, size, idx, wid) for idx, (prop_name, size, wid) in enumerate(json_props_size)]
 
-        self.row_infos = core_stuff
+        self.row_infos = row_infos
 
         # print('✨ DEBUG CORE INFO ✨')
-        # for i, elem in enumerate(core_stuff):
-        #     print(core_stuff[i], i == elem.row_idx, "core_stuff[i]", core_stuff[i] == elem)       
-        return core_stuff
+        # for i, elem in enumerate(row_infos):
+        #     print(row_infos[i], i == elem.row_idx, "row_infos[i]", row_infos[i] == elem)       
+        return row_infos
     
-    def build_chooser_overnames(self, stack_container):
+    def build_chooser_overnames(self): #, stack_container):
         ch = SingleChooser(source=self.load_json_hovernames(), var_kind=None)
-        #self.row_infos[1].set_widget(ch)
         # signal
         ch.get_salient_widget().connect('row-activated', self.handler_chooser_overnames)
         # add to container
-        stack_container.add_named(ch, "widget overnames")
+        self.stack.add_named(ch, "widget overnames")
         return ch
     def handler_chooser_overnames(self, listbox, row):
         name_idx = row.idx
@@ -233,22 +221,22 @@ class InventoryDialog(GimpUi.Dialog, GuiBarGenerator):
         self.set_current_prop([name_idx])
         return True
     
-    def build_chooser_vars(self, stack_container):
+    def build_chooser_vars(self):
         mc = MultiChooser(self.load_json_vars())
         # signal
         for elem in mc.get_listboxes():
            elem.connect('row-activated', self.handler_chooser_vars)
         # add to container
-        stack_container.add_named(mc, "widget_vars")
+        self.stack.add_named(mc, "widget_vars")
         return mc
     def handler_chooser_vars(self, listbox, row):
         print(f"New lbs [{listbox.var_kind}, {row.idx}]")
         self.set_current_prop([listbox.var_kind, row.idx])
-    def build_chooser_kind(self, isMonoChooser, stack_container):
+    def build_chooser_kind(self, isMonoChooser):
         kc = DictChooser(isMonoChooser)
         for elem in kc.get_salient_widgets():
             elem.connect('clicked', self.handler_chooser_kind)
-        stack_container.add_named(kc, "widget_mono" if len(kc.source) == 1 else "widget_kinds")
+        self.stack.add_named(kc, "widget_mono" if len(kc.source) == 1 else "widget_kinds")
         return kc
     def handler_chooser_kind(self, button):
         print(f"Saving kind: {button.key}")
