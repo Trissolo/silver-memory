5bcd7439aa63f5689dbb72c0d27fa84a7066a1bf
Trissolo <00tristano00@gmail.com>
2026-03-22 18:16:47 +0100

    Add array generator for 'textures.addUint8Array'

--- other/external_stuff/gimp_stuff/plugins/tris_game_inventory/invpackage/invgui/dialogazzo.py
+++ other/external_stuff/gimp_stuff/plugins/tris_game_inventory/invpackage/invgui/dialogazzo.py
@@ -9,6 +9,117 @@ from gi.repository import GimpUi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+class PixelBitstream():
+    def __init__(self, layer):
+        self.layer = layer
+        self.x = 0
+        self.y = 0
+        self.width = layer.get_width()
+        self.end = False
+        self.bitstream = []
+    def advance(self):
+        self.x += 1
+        if self.x == self.width:
+            self.x = 0
+            self.y += 1
+    def get_byte(self):
+        byte = 0
+        for i in range(8):
+            value = self.layer.get_pixel(self.x, self.y)
+            byte <<= 1
+            if value is None and not self.end:
+                self.end = True
+            if value and value.get_rgba().red == 1.0:
+                byte |= 1
+            #byte |= 0 if (self.end is True or value.get_rgba().red) == 0.0 else 1
+            self.advance()
+        return self.bitstream.append(byte)
+    def start(self):
+        while not self.end:
+            self.get_byte()
+        self._result(self.bitstream)
+        return self.bitstream
+    def destroy(self):
+        self.layer = None
+        self.bitstream = self.bitstream.clear()
+        return None
+    def _result(self, res):
+        str_stream = "".join([f"{elem:0>8b}" for elem in res])
+        k = self.width
+        chunks = [str_stream[i:i+k] for i in range(0, len(str_stream), k)]
+        for elem in chunks:
+            print(elem)
+        return str_stream
+        
+
+'''
+class Example extends Phaser.Scene
+{
+    base = [255, 129, 185, 165, 165, 185, 129, 255, 0];
+    oriX = 8;
+    oriY = 8;
+
+    create ()
+    {
+        console.log(this.base.length)// === this.oriX * this.oriY)
+        
+        const buffer = new Uint8Array(this.oriX * this.oriY*4);//this.base.length * 4); //this.cache.binary.get('mod');
+        let idx = 0
+        for (const elem of this.base)
+        {
+            //console.log(elem.toString(2))
+            for (const char of elem.toString(2).padStart(8, "0"))
+            {
+                //console.log(char)
+                
+                const val = char === "1"? 255 : 0
+                //console.log(elem, idx++)
+                buffer[idx++] = val;
+                buffer[idx++] = val;
+                buffer[idx++] = val;
+                buffer[idx++] = val;
+            }
+        }
+
+        //console.log(idx, buffer.byteLength)
+        
+        console.log(this.textures.addUint8Array("test", buffer, this.oriX, this.oriY));
+
+        this.add.image(20,20,'test').setOrigin(0).setScale(8);
+
+        return false;
+        
+        const config = {
+            image: 'test',
+            width: 8,
+            height: 8,
+            chars: '0123456789%ab',
+            charsPerRow: 13
+        }
+            
+
+        this.cache.bitmapFont.add('font0', Phaser.GameObjects.RetroFont.Parse(this, config));
+        
+        
+        this.add.bitmapText(10, 10, 'font0', "50%aaab").setScale(3).setOrigin(0);
+    } 
+}
+
+
+const config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);
+'''
+
+
+'''
 # dataclass
 from dataclasses import dataclass
 
@@ -114,3 +225,4 @@ class Dialogazzo(GimpUi.Dialog):
 
 dialogazzo = Dialogazzo()
 #dialogazzo.destroy()
+'''
