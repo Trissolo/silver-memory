fd201468eba381512e33087cbafacc31266363cc
Trissolo <00tristano00@gmail.com>
2026-04-02 12:37:58 +0200

    Tidy up code

--- other/external_stuff/gimp_stuff/plugins/tris_simple_huffman/tris_simple_huffman.py
+++ other/external_stuff/gimp_stuff/plugins/tris_simple_huffman/tris_simple_huffman.py
@@ -61,7 +61,7 @@ class StreamGen():
     def finalize(self):
         if self.idx != 0:
             i = self.idx
-            print(f'Last bit is incomplete. Idx = {i}')
+            print(f'Last bit was incomplete (idx was {i}')
             self.byte <<= (8 - i)
             self.res.append(self.byte)
             self.res.insert(0, i)
@@ -82,6 +82,9 @@ class Node:
 
     def get_char(self):
         return self.char
+    
+    def __repr__(self):
+        return f'[Node] {{is_leaf: {self.is_leaf()}{f", char: {self.char}" if self.char else ""}}}'
 
 
 # 3. the Encoder Class
@@ -128,11 +131,11 @@ class HufEncoder:
         for char in to_process:
             yield char
       
-    def _file_length(complete_path):
-        count = 0
-        for row in open(complete_path, "r"):
-            count += len(row)
-        return count
+    # def _file_length(complete_path):
+    #     count = 0
+    #     for row in open(complete_path, "r"):
+    #         count += len(row)
+    #     return count
 
     def build_huffman_tree(self, text):
         frequency = Counter(text)
@@ -156,6 +159,10 @@ class HufEncoder:
             self.generate_codes(node.right, prefix + "1", huffman_dict)
     
     def preorder_traversal(self, node, writer):  # ENCODE TREE FOR HEADER
+        # print(f"🪝 {node}")
+        # if not node:
+        #     print("🏁 No node: returning")
+        #     return
         if node.is_leaf():
             writer.addBit(1)
             writer.addChar(node.get_char())
@@ -164,15 +171,24 @@ class HufEncoder:
             self.preorder_traversal(node.left, writer)
             self.preorder_traversal(node.right, writer)
 
-        if not node:
-            return
+    def execute(self):
+        self.generate_codes(self.root, "", self.huffman_dict)
         
-    def huffman_encoding(self):
-        text = self.text
-        huffman_dict = self.huffman_dict
-        self.generate_codes(self.root, "", huffman_dict)
-        encoded_text = ''.join(huffman_dict[char] for char in text)
-        return encoded_text
+        self.preorder_traversal(self.root, self.writer)
+
+        for char in ''.join(self.huffman_dict[char] for char in self.text):
+            self.writer.addBit(char)
+        
+        return self.writer.get_output()
+
+        
+        
+    # def huffman_encoding(self):
+    #     text = self.text
+    #     huffman_dict = self.huffman_dict
+    #     self.generate_codes(self.root, "", huffman_dict)
+    #     encoded_text = ''.join(huffman_dict[char] for char in text)
+    #     return encoded_text
                 
     
 
@@ -233,284 +249,31 @@ class TrisHuffman(Gimp.PlugIn):
         print("*** Simple Huffman Encoder Plugin ***")
         options_dialog = GimpUi.ProcedureDialog.new(procedure, config, "title_test")
         options_dialog.fill()
-        options_dialog.run()
-        options_dialog.destroy()
-
+        user_action = options_dialog.run()
+        
         folder = config.get_property(self.ARGU_DEST_FOLDER)
         source_file = config.get_property(self.ARGU_FILE_TO_COMPRESS)
         user_string = config.get_property(self.ARGU_TEST_STRING)
+        options_dialog.destroy()
 
-        print(f'{user_string} ({len(user_string)} chars)')
-                
-
-        #from HERE!
-        test_huff = HufEncoder(source_file, folder, user_string)
-
-        test_huff.generate_codes(test_huff.root, "", test_huff.huffman_dict)
+        # from HERE!
+        if user_action:      
+            test_huff = HufEncoder(source_file, folder, user_string)
         
-        test_huff.preorder_traversal(test_huff.root, test_huff.writer)
+            res = test_huff.execute()
 
-        encoded_text = ''.join(test_huff.huffman_dict[char] for char in test_huff.text)
-
-        for char in encoded_text:
-            test_huff.writer.addBit(char)
+            with open(test_huff.output_path, "wb") as binary_file:
+                binary_file.write(bytes(res))
         
-        print(f'{folder.get_path()}]\n{source_file.get_basename()[:-4]}')
+            Gimp.message(f'Done!\nWrote: {test_huff.output_path}')
 
-        res = test_huff.writer.get_output()
-
-        with open(test_huff.output_path, "wb") as binary_file:
-            binary_file.write(bytes(res))
-        
-        Gimp.message(f'{test_huff.output_path}')   # \n{res}')
-
+        print('(Huffman Done)')
         # do what you want to do, then, in case of success, return:
         return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
     
-    # def read_text(self, file):
-    #     try:
-    #         # Carica il contenuto. Restituisce (successo, byte_content, etag)
-    #         success, content, etag = file.load_contents(None)
-            
-    #         if success:
-    #             # Il contenuto viene restituito come byte, quindi va decodificato in stringa
-    #             text_content = content.decode('utf-8')
-    #             print(text_content)
-    #     except Exception as e:
-    #         print(f"Errore durante la lettura del file: {e}")
-    #def get_content(self, file):
-        success, content, etag = file.load_contents(None)
-
-        # Qui il file è già chiuso lato sistema operativo.
-        # Puoi elaborare 'content' senza preoccuparti di altro.
-        # ['utf-8', 'windows-1252']
-        # print(content.decode('utf-8'))
-    # def cheppa(self, file):
-    #     try:
-    #         # 2. Apri lo stream di lettura (FileInputStream)
-    #         input_stream = file.read(None)
-            
-    #         # 3. Avvolgi lo stream in un DataInputStream per leggere le righe
-    #         data_stream = Gio.DataInputStream.new(input_stream)
-            
-    #         while True:
-    #             # 4. Leggi la riga. Restituisce (linea_in_byte, lunghezza)
-    #             line_bytes, length = data_stream.read_line(None)
-                
-    #             # Se line_bytes è None, abbiamo raggiunto la fine del file (EOF)
-    #             if line_bytes is None:
-    #                 break
-                    
-    #             # Decodifica i byte in stringa (rimuovendo eventuali spazi/newline extra se necessario)
-    #             line_text = line_bytes.decode('utf-8')
-    #             print(line_text)
-
-    #         # Chiudi lo stream una volta finito
-    #         input_stream.close(None)
-
-    #     except Exception as e:
-    #         print(f"Errore durante la lettura: {e}")
 
 Gimp.main(TrisHuffman.__gtype__, sys.argv)
 
-
-
-'''
-console.clear();
-class Node
-{
-    constructor(char = null, left = null, right = null)
-    {
-        this.char = char;
-        this.left = left;
-        this.right = right;
-    }
-
-    is_leaf()
-    {
-        return this.left === null;
-    }
-
-    get_char()
-    {
-        return this.char;
-    }
-}
-
-class DecodeHuffman
-{
-    processedBytes = 0;
-    bitstream;
-  
-    constructor(file)
-    {
-        this.bitstream = this.bitGenerator(file);
-        const total_len = file.length;
-        const padding = this.getAdjacentBits(8)
-        
-        console.log("padding", padding, total_len)
-
-        // skips the control bit set by the compression algorithm
-        //this.getBit();
-
-        // Reconstruct the Huffman tree
-        const root = this.decodeTree();
-
-        // Get the character-code map back
-        const dictionary = this.assign_code(root, '');
-
-        console.log(dictionary);
-   		
-        //const padding = this.getAdjacentBits(8);
-
-        //this.getAdjacentBits(padding);
-
-        const output = [];
-
-        let code = "";
-
-        //let position = {done: false};
-
-        while (this.processedBytes < total_len)
-        {
-            
-            code += this.getBit().value;
-            
-            if (dictionary.has(code))
-            {
-                output.push(dictionary.get(code));
-
-                code = "";
-            }
-        }
-      
-        for (let i = 0; i < padding; i++)
-        {
-          code += this.getBit().value;
-            
-            if (dictionary.has(code))
-            {
-                output.push(dictionary.get(code));
-
-                code = "";
-            }
-        }
-
-        // destroy
-        this.bitstream.return();
-        this.bitstream = undefined;
-        
-        console.log(output.join(''));
-        
-    }
-
-    *bitGenerator(typedArray)
-    {
-        for (const byte of typedArray)
-        {
-          this.processedBytes += 1;
-          //console.log(this.processedBytes);
-            // console.log("Byte", byte);
-            // Iterate bits from Most Significant to Least Significant
-            for (let i = 7; i >= 0; i--)
-            {
-                yield (byte >> i) & 1;
-            }
-          
-        }
-    }
-
-    getBit() 
-    {
-        return this.bitstream.next();
-    }
-    getAdjacentBits(amount)
-    {
-        let bits = 0;
-
-        for (let i = 0; i < amount; i++)
-        {
-            bits <<= 1;
-
-            bits |= this.getBit().value;
-        }
-
-        return bits;
-    }
-
-    decodeTree()
-    {
-        const char = this.getBit().value;
-
-        if (char == 1)
-        {
-            return new Node(String.fromCharCode(this.getAdjacentBits(8)));
-        }
-
-        else
-        {
-            const left = this.decodeTree();
-            const right = this.decodeTree();
-            return new Node(null, left, right);
-        }
-    }
-
-    assign_code(node, code = '')
-    {
-        if (node.is_leaf())
-        {
-            return new Map().set(code, node.get_char());
-        }
-
-        return new Map([...this.assign_code(node.left, code + '0'), ...this.assign_code(node.right, code + '1')]);
-        
-    }
-}
-class TestScene extends Phaser.Scene
-{
- 
-  constructor ()
-  {
-    super({ key: 'TestScene' });
-  }
-
-preload()
-    {
-        this.load.binary('mio', 'assets/promessi.thf', Uint8Array);
-        // this.load.binary('mio', 'assets/lorem.bin', Uint8Array);
-        // this.load.binary('mio', 'assets/large_text.bin', Uint8Array);
-        // this.load.binary('mio', 'assets/commedia.bin', Uint8Array)
-    }
-
-    create()
-    {
-       new DecodeHuffman(this.cache.binary.get('mio'));
-    }
-} // end TestScene
-    
-const config = {
-    type: Phaser.WEBGL,
-    parent: "gameContainer",
-    pixelArt: true,
-    backgroundColor: '#320822',
-    scale: {
-        mode: Phaser.Scale.NONE,
-        //autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 200,
-        height: 200,
-        zoom: 2
-    },
-    //loader: {
-    //  baseURL: 'https://labs.phaser.io/',
-    //  baseURL: 'https://i.ibb.co/YhGPn4S',
-    //  crossOrigin: 'anonymous'
-    //},
-    scene: [TestScene]
-};
-
-window.game = new Phaser.Game(config)
-
-'''
 
 '''
 console.clear();
@@ -578,42 +341,35 @@ class DecodeHuffman
         {
             process()    
         }
-      
+
         if (padding)
         {
-          while (padding--)
-          {
-              process()
-          }
+            while (padding--)
+            {
+                process()
+            }
         }
       
-//         for (let i = 0; i < padding; i++)
-//         {
-//             process()
-//         }
-
         // destroy
         this.bitstream.return();
       
         this.bitstream = undefined;
       
         // manage output
-        console.log(output.join(''));
-        
+        console.log(output.join('')); 
     }
 
     *bitGenerator(typedArray)
     {
         for (const byte of typedArray)
         {
-          this.processedBytes += 1;
+            this.processedBytes += 1;
           
             // Iterate bits from Most Significant to Least Significant
             for (let i = 7; i >= 0; i--)
             {
                 yield (byte >> i) & 1;
             }
-          
         }
     }
 
@@ -621,6 +377,7 @@ class DecodeHuffman
     {
         return this.bitstream.next();
     }
+
     getAdjacentBits(amount)
     {
         let bits = 0;
@@ -662,7 +419,8 @@ class DecodeHuffman
         return new Map([...this.assign_code(node.left, code + '0'), ...this.assign_code(node.right, code + '1')]);
         
     }
-}
+}  // End DecodeHuffman
+
 class TestScene extends Phaser.Scene
 {
  
@@ -675,6 +433,7 @@ preload()
     {
         //this.load.binary('mio', 'assets/promessi.thf', Uint8Array);
         this.load.binary('mio', 'assets/coso.thf', Uint8Array);
+        // this.load.binary('mio', 'assets/default.thf', Uint8Array);
         // this.load.binary('mio', 'assets/lorem.bin', Uint8Array);
         // this.load.binary('mio', 'assets/large_text.bin', Uint8Array);
         // this.load.binary('mio', 'assets/commedia.bin', Uint8Array)
